12c41480dd1a0cb5e6aa21087b8f5772c0242f6b
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2022-06-16 23:17:01 +0900

    🚧 test page の作成
    showcase のテスト

---
+++ pages/test.vue
@@ -0,0 +1,16 @@
+<script setup>
+const res = useFetch('https://back-end.hackathon-22-spring-16.trap.show/echo')
+onMounted(() => {
+  console.log(res)
+  console.log(res.data)
+})
+</script>
+
+<template>
+  <div>
+    <h1>This is Test Page</h1>
+    <div>
+      {{ res.data }}
+    </div>
+  </div>
+</template>
