eff9a93da035fc6b7d78497b01d6b198433d7021
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2022-06-16 23:57:36 +0900

    🚧 cookie 渡さないとだめだったかも

--- pages/test.vue
+++ pages/test.vue
@@ -1,12 +1,14 @@
 <script setup>
-const res = await useFetch('https://back-end.hackathon-22-spring-16.trap.show/echo')
+const res = await useFetch('https://back-end.hackathon-22-spring-16.trap.show/echo', {
+  headers: useRequestHeaders([('cookie')])
+})
 onMounted(async () => {
-  console.log('response', res)
-  console.log('response.data', res.data)
+  console.log('response', res.value)
+  console.log('response.data', res.data.value)
   console.log('start refresh ...')
   await res.refresh()
-  console.log('response', res)
-  console.log('response.data', res.data)
+  console.log('response', res.value)
+  console.log('response.data', res.data.value)
 })
 </script>
 
