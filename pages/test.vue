cee2073be4245a01a7035a4b976f6e290bfd15c3
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2022-06-17 00:13:31 +0900

    🚧 お願いします

--- pages/test.vue
+++ pages/test.vue
@@ -1,6 +1,6 @@
 <script setup>
 const res = await useFetch('https://back-end.hackathon-22-spring-16.trap.show/echo', {
-  headers: useRequestHeaders([('cookie')])
+  headers: useRequestHeaders([('cookie')]) || useCookies()
 })
 onMounted(async () => {
   console.log('response', res.value)
@@ -10,6 +10,10 @@ onMounted(async () => {
   console.log('response', res.value)
   console.log('response.data', res.data.value)
 })
+const onClick = async () => {
+  const { data } = await useFetch('https://back-end.hackathon-22-spring-16.trap.show/echo')
+  console.log('response', data.value)
+}
 </script>
 
 <template>
@@ -18,5 +22,6 @@ onMounted(async () => {
     <div>
       {{ res.data }}
     </div>
+    <v-btn @click="onClick">click</v-btn>
   </div>
 </template>
