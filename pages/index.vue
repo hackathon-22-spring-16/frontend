78c6a79e45068e62306eba16240c4ee8ade1494a
tajiken22 <rounin.toukou@gmail.com>
2022-06-18 15:27:45 +0900

    v-modelの実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,29 @@
 vue
 <script setup lang="ts">
+import { ref } from "vue";
 import CodeEditor from '@/components/CodeEditor.vue'
 
+
 const count = ref(0)
+
+const code = ref('')
+
+const onClick = () => {
+  console.log(code.value)
+}
+
 </script>
 
 <template>
   <v-card>
     <v-card-title> counter </v-card-title>
     <v-card-text>
+      <v-btn @click="onClick">ボタン</v-btn>
       <v-btn @click="count++"> count: {{ count }} </v-btn>
       <nuxt-link to="/editor">test</nuxt-link>
-      <code-editor />
+      <code-editor v-model="code"/>
     </v-card-text>
   </v-card>
+
+  
 </template>
