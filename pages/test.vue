85abb484e8e8a45eeabc60347425201b624f0d56
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2022-06-19 14:52:18 +0900

    🚧 🤔🤔🤔🤔🤔🤔🤔

--- pages/test.vue
+++ pages/test.vue
@@ -1,35 +1,17 @@
 <script setup>
 const res = await useFetch(
-  'https://back-end.hackathon-22-spring-16.trap.show/echo',
-  {
-    headers: useRequestHeaders(['cookie']) || useCookies(),
-    credentials: 'include',
-  }
+  `http://brain-t.api.trap.games/get-code/${route.params.username}/${route.params.hash}`
 )
-onMounted(async () => {
-  console.log('response', res.value)
-  console.log('response.data', res.data.value)
-  console.log('start refresh ...')
-  await res.refresh()
-  console.log('response', res.value)
-  console.log('response.data', res.data.value)
+onMounted(() => {
+  console.log(res)
 })
-const onClick = async () => {
-  const { data } = await useFetch(
-    'https://back-end.hackathon-22-spring-16.trap.show/echo',
-    {
-      credentials: 'include',
-    }
-  )
-  console.log('response', data.value)
-}
 </script>
 
 <template>
   <div>
     <h1>This is Test Page</h1>
     <div>
-      {{ res.data }}
+      {{ res }}
     </div>
     <v-btn @click="onClick">click</v-btn>
   </div>
